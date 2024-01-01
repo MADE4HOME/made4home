9584d6c5af2b5692aa1c5dd5597d42d30e7636f0
Orlin Dimitrov <orlin369@gmail.com>
2024-01-01 18:49:41 +0200

    Add modbud ethernet TCP master example

--- examples/ethernet_modbus_tcp_master/ethernet_modbus_tcp_master.ino
+++ examples/ethernet_modbus_tcp_master/ethernet_modbus_tcp_master.ino
@@ -28,11 +28,13 @@ SOFTWARE.
 
 #define UPDATE_INTERVAL_MS 1000
 
-#define MB_BAUDRATE 9600
+#define MB_PORT 502
 
 #define MB_SLAVE_ID 2
 
-#define MB_TIMEOUT_MS 500
+#define MB_TIMEOUT_MS 2000
+
+#define MB_INTERVAL_MS 200
 
 #define MB_REG_START 0
 
@@ -42,36 +44,69 @@ SOFTWARE.
 
 #pragma region Headers
 
-#include "made4home.h"
-#include "FxTimer.h"
+#include <ETH.h>
 
-#include "ModbusClientRTU.h"
+#include <WiFi.h>
+
+#include "made4home.h"
+
+// Include the header for the ModbusClient TCP style
+#include "ModbusClientTCP.h"
+// Include the header for the coils data type
+#include "CoilData.h"
 #include "Logging.h"
+
+#include "FxTimer.h"
 
 #pragma endregion
 
 #pragma region Variables
 
 /**
- * @brief Request time.
+ * @brief WiFi client.
  * 
  */
-uint32_t RequestTime_g;
+WiFiClient WiFiClient_g;
 
 /**
- * @brief The RS485 module has no half-duplex, so the parameter with the DE/RE pin is required!
+ * @brief 
  * 
  */
-ModbusClientRTU *ModbusClientRTU_g;
+IPAddress TargetIP_g(172, 33, 4, 10);
+
+/**
+ * @brief Create a ModbusTCP client instance.
+ * 
+ */
+ModbusClientTCP *ModbusClient_g;
 
 /** 
  * @brief Update timer instance.
  */
 FxTimer *UpdateTimer_g;
 
+/**
+ * @brief Coil data.
+ * 
+ */
+CoilData CoilData_g(12);
+
+/**
+ * @brief Ethernet connection state.
+ * 
+ */
+static bool EthernetConnected_g = false;
+
 #pragma endregion
 
 #pragma region Prototypes
+
+/**
+ * @brief Network event state handler.
+ * 
+ * @param event Event input.
+ */
+void wifi_event(WiFiEvent_t event);
 
 /**
  * @brief Define an onData handler function to receive the regular responses
@@ -97,62 +132,79 @@ void setup()
     Serial.begin(DEFAULT_BAUDRATE, SERIAL_8N1);
     while (!Serial) {}
 
-    // Setup the board IO.
     Made4Home.setup();
    
-    // Set up Serial2 connected to Modbus RTU
-    RTUutils::prepareHardwareSerial(Serial2);
-    Serial2.begin(MB_BAUDRATE, SERIAL_8N1, PIN_RS485_RX, PIN_RS485_TX);
+    // Attach the network events.
+    WiFi.onEvent(wifi_event);
+
+    // Run the Ethernet.
+    ETH.begin(
+        PIN_ETH_PHY_ADDR,
+        PIN_ETH_PHY_POWER,
+        PIN_ETH_PHY_MDC,
+        PIN_ETH_PHY_MDIO,
+        PIN_ETH_PHY_TYPE,
+        PIN_ETH_CLK_MODE);
+
+    ModbusClient_g = new ModbusClientTCP(WiFiClient_g);
 
     // Set up ModbusRTU client.
-    ModbusClientRTU_g = new ModbusClientRTU(PIN_RS485_EN);
-
     // - provide onData handler function
-    ModbusClientRTU_g->onResponseHandler(&handleResponse);
+    ModbusClient_g->onResponseHandler(&handleResponse);
     // - provide onError handler function
-    ModbusClientRTU_g->onErrorHandler(&handleError);
+    ModbusClient_g->onErrorHandler(&handleError);
     // Set message timeout to 500ms
-    ModbusClientRTU_g->setTimeout(MB_TIMEOUT_MS);
-    // Start ModbusRTU background task
-    ModbusClientRTU_g->begin(Serial2);
+    ModbusClient_g->setTimeout(MB_TIMEOUT_MS, MB_INTERVAL_MS);
+    // Start ModbusTCP background task
+    ModbusClient_g->begin();
+    // Issue a request
+    // Set Modbus TCP server address and port number
+    // (Fill in your data here!)
+    ModbusClient_g->setTarget(TargetIP_g, MB_PORT);
 
     // Setup the update timer.
     UpdateTimer_g = new FxTimer();
     UpdateTimer_g->setExpirationTime(UPDATE_INTERVAL_MS);
     UpdateTimer_g->updateLastTime();
+
+    // Setup default value of the coils.
+    CoilData_g = "000000000000";
 }
 
 void loop()
 {
+    static Error ErrorL;
+
     UpdateTimer_g->update();
     if(UpdateTimer_g->expired())
     {
         UpdateTimer_g->updateLastTime();
         UpdateTimer_g->clear();
-        
+
         // Issue the request
-        Error ErrorL = ModbusClientRTU_g->addRequest((uint32_t)millis(), MB_SLAVE_ID, READ_DISCR_INPUT, MB_REG_START, MB_REG_COUNT);
+        ErrorL = ModbusClient_g->addRequest((uint32_t)millis(), MB_SLAVE_ID, READ_DISCR_INPUT, MB_REG_START, MB_REG_COUNT);
         if (ErrorL!=SUCCESS)
         {
-            ModbusError e(ErrorL);
-            LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
+          ModbusError e(ErrorL);
+          LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
         }
-        
+
         for (int index = 0; index < PINS_INPUTS_COUNT; index++)
         {
-            // Issue the request
-            Error ErrorL1 = ModbusClientRTU_g->addRequest(
-                (uint32_t)millis(),
-                MB_SLAVE_ID,
-                WRITE_COIL,
-                index,
-                Made4Home.digitalRead(index) ? 0xFF00 : 0x0000);
-            
-            if (ErrorL1!=SUCCESS)
-            {
-              ModbusError e(ErrorL1);
-              LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
-            }              
+            CoilData_g.set(index, (Made4Home.digitalRead(index) == HIGH));
+        }
+
+        // Issue the request
+        ErrorL = ModbusClient_g->addRequest(
+            (uint32_t)millis(),
+            MB_SLAVE_ID,
+            WRITE_MULT_COILS,
+            0, CoilData_g.coils(), CoilData_g.size(), CoilData_g.data());
+
+        if (ErrorL!=SUCCESS)
+        {
+          ModbusError e(ErrorL);
+          LOG_E("Error creating request: %02X - %s\n", (int)e, (const char *)e);
         }
     }
 }
@@ -160,11 +212,49 @@ void loop()
 #pragma region Functions
 
 /**
- * @brief Define an onData handler function to receive the regular responses
+ * @brief Network event state handler.
  * 
- * @param response Received response message.
- * @param token Request's token.
+ * @param event Event input.
  */
+void wifi_event(WiFiEvent_t event)
+{
+    switch (event)
+    {
+    case ARDUINO_EVENT_ETH_START:
+        Serial.println("ETH Started");
+        //set eth hostname here
+        ETH.setHostname("made4home");
+        break;
+    case ARDUINO_EVENT_ETH_CONNECTED:
+        Serial.println("ETH Connected");
+        break;
+    case ARDUINO_EVENT_ETH_GOT_IP:
+        Serial.print("ETH MAC: ");
+        Serial.print(ETH.macAddress());
+        Serial.print(", IPv4: ");
+        Serial.print(ETH.localIP());
+        if (ETH.fullDuplex())
+        {
+            Serial.print(", FULL_DUPLEX");
+        }
+        Serial.print(", ");
+        Serial.print(ETH.linkSpeed());
+        Serial.println("Mbps");
+        EthernetConnected_g = true;
+        break;
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
+        Serial.println("ETH Disconnected");
+        EthernetConnected_g = false;
+        break;
+    case ARDUINO_EVENT_ETH_STOP:
+        Serial.println("ETH Stopped");
+        EthernetConnected_g = false;
+        break;
+    default:
+        break;
+    }
+}
+
 void handleResponse(ModbusMessage response, uint32_t token) 
 {
     static uint8_t FunctionCodeL = 0;
@@ -176,12 +266,23 @@ void handleResponse(ModbusMessage response, uint32_t token)
     // If it is READ_DISCR_INPUT
     if (FunctionCodeL == READ_DISCR_INPUT)
     {
-        InputsL = response[3];     
+        InputsL = response[3];
+        Serial.printf("Inputs: %d", InputsL);
+        Serial.println();
+      
         Made4Home.digitalWrite(0, (InputsL & 0x01) ? 1 : 0);
         Made4Home.digitalWrite(1, (InputsL & 0x02) ? 1 : 0);
         Made4Home.digitalWrite(2, (InputsL & 0x04) ? 1 : 0);
         Made4Home.digitalWrite(3, (InputsL & 0x08) ? 1 : 0);
+
+        // for(uint16_t index = 0; index < response.size(); index++)
+        // {
+        //     Serial.printf("%02X: %02X, ", index, response[index]);
+        // }
+        // Serial.println();
     }
+
+    Serial.println("handleResponse");
 }
 
 /**
